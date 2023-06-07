4784cf267a34a37f397e06554de2641995b296c4
Nick Adair <adair8@purdue.edu>
2023-06-07 07:56:22 -0400

    test commit and push

--- swift_intro/ContentView.swift
+++ swift_intro/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello")
+            Text("nick is the best")
         }
         .padding()
     }
