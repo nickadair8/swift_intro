c075860eaec8acc90e33a48cd01c1be3f71ce825
Nick Adair <adair8@purdue.edu>
2023-06-07 07:51:27 -0400

    Initial Commit

---
+++ swift_intro/swift_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_introApp.swift
+//  swift_intro
+//
+//  Created by Nick A on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
